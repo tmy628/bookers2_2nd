a028e931f8c5998855153d4080e9e0d69e22d9fe
tmy628 <tmy6h1r@gmail.com>
2021-04-19 06:02:56 +0000

    CRUD機能を実装 #18

--- bookers2/app/controllers/application_controller.rb
+++ bookers2/app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+    # ログイン後は（自分の投稿一覧）に遷移する
+  end
+
   protected
 
   def configure_permitted_parameters
